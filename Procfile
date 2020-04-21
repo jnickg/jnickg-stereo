aff4c256ace9fe5e00ac3e9c1df4523e5d1b6598
jnickg <jnickg83@gmail.com>
2020-04-20 20:52:54 -0700

    Convert boilerplate to starter for rectifi

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn stereo.wsgi --log-file -
--- rectifi/views.py
+++ rectifi/views.py
@@ -6,10 +6,10 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
-    # return HttpResponse('Hello from Python!')
+    #r = requests.get('http://httpbin.org/status/420')
+    #print(r.text)
+    #return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
 
 
